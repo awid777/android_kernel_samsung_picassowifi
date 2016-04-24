dacc3e323ccbdc0288a736e44728bd77983a2428
Ethan Chen <intervigil@gmail.com>
2016-04-23 22:04:37 -0500

    [media] fimc: Move camera ID file to sane location
    Change-Id: I9b3bb7b5cdfd0f2fa3d55fd5ae0c8195cefddd4b

--- drivers/media/video/exynos/fimc-is-mc2/fimc-is-sec-define.h
+++ drivers/media/video/exynos/fimc-is-mc2/fimc-is-sec-define.h
@@ -85,25 +85,34 @@
 #define FW_IMX134	"E08LS"
 
 #define SDCARD_FW
-#define FIMC_IS_SETFILE_SDCARD_PATH		"/data/media/0/"
+#define FIMC_IS_SETFILE_SDCARD_PATH		"/data/camera/"
 #define FIMC_IS_FW				"fimc_is_fw2.bin"
 #define FIMC_IS_FW_3L2				"fimc_is_fw2_3L2.bin"
-#define FIMC_IS_FW_IMX134			"fimc_is_fw2_IMX134.bin"
-#define FIMC_IS_FW_SDCARD			"/data/media/0/fimc_is_fw2.bin"
+#define FIMC_IS_FW_IMX134
+#define FIMC_IS_FW_IMX134_EEPROM		"fimc_is_fw2_IMX134_EEPROM.bin"
+#define FIMC_IS_FW_SDCARD			"/data/camera/fimc_is_fw2.bin"
 #define FIMC_IS_IMX135_SETF			"setfile_imx135.bin"
 #define FIMC_IS_IMX134_SETF			"setfile_imx134.bin"
 #define FIMC_IS_3L2_SETF			"setfile_3L2.bin"
 #define FIMC_IS_6B2_SETF			"setfile_6b2.bin"
 #define FIMC_IS_FW_PATH				"/system/vendor/firmware/"
-#define FIMC_IS_FW_DUMP_PATH			"/data/"
+#define FIMC_IS_FW_DUMP_PATH			"/data/camera/"
 
 #define FIMC_IS_FW_BASE_MASK			((1 << 26) - 1)
 #define FIMC_IS_VERSION_SIZE			42
 #define FIMC_IS_SETFILE_VER_OFFSET		0x40
 #define FIMC_IS_SETFILE_VER_SIZE		52
 
-#define FIMC_IS_CAL_SDCARD			"/data/cal_data.bin"
 /*#define FIMC_IS_MAX_CAL_SIZE			(20 * 1024)*/
+#define FIMC_IS_CAL_SDCARD			"/data/camera/cal_data.bin"
+
+#ifdef CONFIG_CAMERA_EEPROM
+#define FIMC_IS_MAX_FW_SIZE			(8 * 1024)
+#define HEADER_CRC32_LEN			(80 / 2)
+#define OEM_CRC32_LEN				(64 / 2)
+#define AWB_CRC32_LEN				(32 / 2)
+#define SHADING_CRC32_LEN			(6623 / 2)
+#else
 #define FIMC_IS_MAX_FW_SIZE			(2048 * 1024)
 #define FIMC_IS_CAL_START_ADDR			(0x013D0000)
 #define FIMC_IS_CAL_RETRY_CNT			(2)
